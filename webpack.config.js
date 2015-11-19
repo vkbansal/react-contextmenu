2ad86c684db20b48378ca3dbfc382ccbae7658b2
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-11-19 07:13:17 +0530

    Add react-dom to external dependency

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,12 @@ module.exports = {
             commonjs2: "react",
             commonjs: "react",
             amd: "react"
+        },
+        "react-dom": {
+            root: "ReactDOM",
+            commonjs2: "react-dom",
+            commonjs: "react-dom",
+            amd: "react-dom"
         }
     }],
     plugins: [
