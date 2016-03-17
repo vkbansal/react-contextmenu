b82b23bb024856bbb100e7ba5fac88be5f305784
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-03-17 08:44:18 +0530

    add connect helper

--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ export { default as ContextMenuLayer } from "./contextmenu-layer";
 export { default as MenuItem } from "./menu-item";
 export { default as monitor } from "./monitor";
 export { default as SubMenu } from "./submenu";
+export { default as connect } from "./connect";
