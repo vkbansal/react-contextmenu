de6e17d446f22c29eff6f7e7e48925eb4e645591
Vivek Kumar Bansal <vkb0310@gmail.com>
2016-03-26 10:11:01 +0530

    update travis settings

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,23 @@
 
 set -o errexit
 
+# reset public dir
 rm -rf public
 mkdir public
+
+# copy required files
 cp ./examples/index.html ./public/index.html
 cp ./examples/react-context-menu.css ./public/react-context-menu.css
 npm run examples && cp ./bundle.js ./public/bundle.js
+
+
+cd public
+git init
+
+git config user.name "Travis CI"
+git config user.name "vkb0310@gmail.com"
+
+git add .
+git commit -m "Deploy to gh-pages"
+
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
