8c15558903932872f5ac30f2afb800f1c47ae792
zhili.wzl <zhili.wzl@alibaba-inc.com>
2016-12-02 13:08:13 +0800

    prevent trigger in dragging

--- src/ContextMenuTrigger.js
+++ src/ContextMenuTrigger.js
@@ -22,9 +22,13 @@ export default class ContextMenuTrigger extends Component {
 
 
     handleMouseDown = (event) => {
+        this.mousePosition = {
+            x:event.pageX,
+            y:event.pageY
+        }
         if (this.props.holdToDisplay >= 0 && event.button === 0) {
             event.persist();
-
+            
             this.mouseDownTimeoutId = setTimeout(
                 () => this.handleContextClick(event),
                 this.props.holdToDisplay
@@ -49,26 +53,37 @@ export default class ContextMenuTrigger extends Component {
         }
     }
 
+
     handleTouchEnd = (event) => {
         event.preventDefault();
         clearTimeout(this.touchstartTimeoutId)
+    }
+
+    handleMouseMove = (event)=>{
+        event.persist()
+        this.mouseMoveEvent = event
     }
 
     handleContextClick = (event) => {
         event.preventDefault();
         event.stopPropagation();
 
+        event = this.mouseMoveEvent || event
+
         const x = event.clientX || (event.touches && event.touches[0].pageX);
         const y = event.clientY || (event.touches && event.touches[0].pageY);
-
+        
         hideMenu();
 
-        showMenu({
-            position: {x, y},
-            target: this.elem,
-            id: this.props.id,
-            data: callIfExists(this.props.collect, this.props)
-        });
+        if(this.mousePosition.x == x && this.mousePosition.y == y){
+            showMenu({
+                position: {x, y},
+                target: this.elem,
+                id: this.props.id,
+                data: callIfExists(this.props.collect, this.props)
+            });
+        }
+        
     }
 
     elemRef = (c) => {
@@ -82,6 +97,7 @@ export default class ContextMenuTrigger extends Component {
             onContextMenu: this.handleContextClick,
             onMouseDown: this.handleMouseDown,
             onMouseUp: this.handleMouseUp,
+            onMouseMove:this.handleMouseMove,
             onTouchStart: this.handleTouchstart,
             onTouchEnd: this.handleTouchEnd,
             onMouseOut: this.handleMouseUp,
