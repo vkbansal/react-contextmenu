d564b5f5002afef9af3ed2c653dad820170faf9e
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-02-18 15:48:43 +0530

    add actions test

---
+++ tests/actions.test.js
@@ -0,0 +1,39 @@
+import { showMenu , hideMenu, MENU_SHOW, MENU_HIDE } from '../src/actions';
+
+
+let showCallback = null;
+let hideCallback = null;
+let eventObj = {};
+
+describe('actions test', () => {
+    beforeAll(() => {
+        showCallback = jest.fn((e) => {
+            eventObj = e;
+            console.log('I\'ll be called on show')
+        });
+        window.addEventListener(MENU_SHOW, showCallback);
+
+        hideCallback = jest.fn((e) => {
+            eventObj = e;
+            console.log('I\'ll be called on hide')
+        });
+        window.addEventListener(MENU_HIDE, hideCallback);
+    });
+
+    test('showMenu', () => {
+        showMenu();
+        expect(showCallback).toHaveBeenCalled();
+        expect(eventObj.type).toBe(MENU_SHOW);
+    });
+
+    test('hideMenu', () => {
+        hideMenu();
+        expect(hideCallback).toHaveBeenCalled();
+        expect(eventObj.type).toBe(MENU_HIDE);
+    });
+
+    afterAll(() => {
+        window.removeEventListener(MENU_SHOW, showCallback);
+        window.removeEventListener(MENU_HIDE, hideCallback);
+    });
+});
