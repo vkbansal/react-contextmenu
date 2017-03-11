2ef4aae1aa624275f1c9f50d67417d39b2788d22
Carsten Wickner <CarstenWickner@users.noreply.github.com>
2017-03-11 04:23:34 +0100

    Pass trigger props to menu renderer (#65) (#83)
    * Pass trigger props to menu renderer (#65
    
    * Solve eslint errors on connectMenu.js and its example DynamicMenu.js
    
    * Solve eslint errors regarding single line blocks
    
    * Revert "Solve eslint errors regarding single line blocks"
    
    This reverts commit 694406114342645403d221137cc8b7ad0d90a782.
    
    * Fix connectMenu and its example
    
    * Add disabled style for examples
    
    * Forward id via connectMenu()
    
    * Consistent usage of template strings in example

---
+++ examples/DynamicMenu.js
@@ -0,0 +1,114 @@
+import React, { Component, PropTypes } from 'react';
+
+import ContextMenuTrigger from 'src/ContextMenuTrigger';
+import ContextMenu from 'src/ContextMenu';
+import MenuItem from 'src/MenuItem';
+import connectMenu from 'src/connectMenu';
+
+const MENU_TYPE = 'DYNAMIC';
+
+const targets = [{
+    name: 'Banana'
+}, {
+    name: 'Apple'
+}, {
+    name: 'Papaya'
+}, {
+    name: 'Mango'
+}, {
+    name: 'Orange'
+}, {
+    name: 'Pineapple'
+}];
+
+function collect(props) {
+    return props;
+}
+
+const DynamicMenu = (props) => {
+    const { id, trigger } = props;
+    const handleItemClick = trigger ? trigger.onItemClick : null;
+
+    return (
+        <ContextMenu id={id}>
+            {trigger && <MenuItem onClick={handleItemClick} data={{action: 'Added'}}>{`Add 1 ${trigger.name}`}</MenuItem>}
+            {trigger && (
+                trigger.allowRemoval
+                    ? <MenuItem onClick={handleItemClick} data={{action: 'Removed'}}>{`Remove 1 ${trigger.name}`}</MenuItem>
+                    : <MenuItem disabled>{'Removal disabled'}</MenuItem>
+            )}
+        </ContextMenu>
+    );
+};
+
+DynamicMenu.propTypes = {
+    id: PropTypes.string.isRequired,
+    trigger: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        onItemClick: PropTypes.func.isRequired,
+        allowRemoval: PropTypes.bool
+    })
+};
+
+const ConnectedMenu = connectMenu(MENU_TYPE)(DynamicMenu);
+
+export default class DynamicMenuExample extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { logs: [] };
+    }
+
+    handleClick = (e, data, target) => {
+        const count = parseInt(target.getAttribute('data-count'), 10);
+
+        if (data.action === 'Added') {
+            target.setAttribute('data-count', count + 1);
+
+            return this.setState(({logs}) => ({
+                logs: [`${data.action} 1 ${data.name}`, ...logs]
+            }));
+        }
+        if (data.action === 'Removed' && count > 0) {
+            target.setAttribute('data-count', count - 1);
+
+            return this.setState(({logs}) => ({
+                logs: [`${data.action} 1 ${data.name}`, ...logs]
+            }));
+        }
+        this.setState(({logs}) => ({
+            logs: [` ${data.name} cannot be ${data.action.toLowerCase()}`, ...logs]
+        }));
+    }
+
+    render() {
+        const attributes = {
+            'data-count': 0,
+            className: 'example-multiple-targets well'
+        };
+
+        return (
+            <div>
+                <h3>Dynamic Menu</h3>
+                <p>This demo shows usage of dynamically created menu on multiple targets.</p>
+                <div className='row'>
+                    {targets.map((item, i) => (
+                        <div key={i} className='col-sm-2 text-center'>
+                            <ContextMenuTrigger
+                                id={MENU_TYPE} holdToDisplay={1000}
+                                name={item.name} onItemClick={this.handleClick}
+                                allowRemoval={i % 2 === 0}
+                                collect={collect} attributes={attributes}>
+                                {item.name}
+                            </ContextMenuTrigger>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    {this.state.logs.map((log, i) => (<p key={i}>{log}</p>))}
+                </div>
+                <ConnectedMenu />
+            </div>
+        );
+    }
+}
---
+++ src/connectMenu.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import ContextMenuTrigger from './ContextMenuTrigger';
+import listener from './globalEventListener';
+
+// collect ContextMenuTrigger's expected props to NOT pass them on as part of the context
+const ignoredTriggerProps = [...Object.keys(ContextMenuTrigger.propTypes), 'children'];
+
+// expect the id of the menu to be responsible for as outer parameter
+export default function (menuId) {
+    // expect menu component to connect as inner parameter
+    // <Child/> is presumably a wrapper of <ContextMenu/>
+    return function (Child) {
+        // return wrapper for <Child/> that forwards the ContextMenuTrigger's additional props
+        return class ConnectMenu extends Component {
+            constructor(props) {
+                super(props);
+                this.state = { trigger: null };
+            }
+
+            componentDidMount() {
+                this.listenId = listener.register(this.handleShow, this.handleHide);
+            }
+
+            componentWillUnmount() {
+                if (this.listenId) {
+                    listener.unregister(this.listenId);
+                }
+            }
+
+            handleShow = (e) => {
+                if (e.detail.id !== menuId) return;
+
+                // the onShow event's detail.data object holds all ContextMenuTrigger props
+                const { data } = e.detail;
+                const filteredData = {};
+
+                for (const key in data) {
+                    // exclude props the ContextMenuTrigger is expecting itself
+                    if (!ignoredTriggerProps.includes(key)) {
+                        filteredData[key] = data[key];
+                    }
+                }
+                this.setState({ trigger: filteredData });
+            }
+
+            handleHide = () => {
+                this.setState({ trigger: null });
+            }
+
+            render() {
+                return <Child {...this.props} id={menuId} trigger={this.state.trigger} />;
+            }
+        };
+    };
+}
