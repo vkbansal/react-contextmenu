345d21bd9f749819b08b83c35ed1c0336f68ad4d
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-05-11 08:12:33 +0530

    fix for tests

--- tests/.setup.js
+++ tests/.setup.js
@@ -1,8 +1,9 @@
 const jsdom = require('jsdom');
 
 const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
-global.document = jsdom.jsdom(documentHTML);
-global.window = document.parentWindow;
+const dom = new jsdom.JSDOM(documentHTML);
+global.document = dom.window.document;
+global.window = dom.window;
 global.window.resizeTo = (width, height) => {
     global.window.innerWidth = width || global.window.innerWidth;
     global.window.innerHeight = width || global.window.innerHeight;
