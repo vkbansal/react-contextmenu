6384fc4013fd1767c5505acbc4bb000397c190d8
Daniel Crisan <crisan@hpe.com>
2017-07-20 00:37:54 +0300

    export show/hideMenu methods in order to customize the trigger

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ export { default as ContextMenuTrigger } from './ContextMenuTrigger';
 export { default as MenuItem } from './MenuItem';
 export { default as SubMenu } from './SubMenu';
 export { default as connectMenu } from './connectMenu';
+export { hideMenu as hideMenu } from './actions';
+export { showMenu as showMenu } from './actions'; 
