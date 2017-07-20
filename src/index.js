b850d427b12358ae7a923fad65a0983842548bac
Daniel Crisan <crisan@hpe.com>
2017-07-20 09:20:52 +0300

    fix lint issues 2

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ export { default as ContextMenuTrigger } from './ContextMenuTrigger';
 export { default as MenuItem } from './MenuItem';
 export { default as SubMenu } from './SubMenu';
 export { default as connectMenu } from './connectMenu';
-export { hideMenu, showMenu  } from './actions';
+export { hideMenu, showMenu } from './actions';
